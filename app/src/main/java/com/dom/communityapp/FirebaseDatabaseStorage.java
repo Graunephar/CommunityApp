261d3383785af1d5b74263c12acdecba4bbe6367
MRL <mrlassen86@gmail.com>
2017-12-07 15:34:37 +0100

    Added context to progress Dialog.

--- app/src/main/java/com/dom/communityapp/FirebaseDatabaseStorage.java
+++ app/src/main/java/com/dom/communityapp/FirebaseDatabaseStorage.java
@@ -1,8 +1,9 @@
 package com.dom.communityapp;
 
 
-
+import android.app.ProgressDialog;
 import android.support.annotation.NonNull;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -32,10 +33,9 @@ public class FirebaseDatabaseStorage {
 
         if (uploadActivity.filePath != null) {
 
-            /*final ProgressDialog progressDialog = new ProgressDialog();
-                      progressDialog.setTitle("Uploading");
-                    progressDialog.show();
-*/
+            final ProgressDialog progressDialog = new ProgressDialog(uploadActivity);
+            progressDialog.setTitle("Uploading");
+            progressDialog.show();
 
 
             StorageReference imageRef = storageReference.child("images/" + uploadActivity.filePath.getLastPathSegment());
@@ -44,24 +44,24 @@ public class FirebaseDatabaseStorage {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
 
-                                                     /*progressDialog.dismiss();
-                           Toast.makeText(context, "File Uploaded ", Toast.LENGTH_LONG).show();
-                            Toast.makeText(getApplicationContext(), "File Uploaded ", Toast.LENGTH_LONG).show();*/
+                            progressDialog.dismiss();
+                            Toast.makeText(uploadActivity, "File Uploaded ", Toast.LENGTH_LONG).show();
+                            Toast.makeText(uploadActivity, "File Uploaded ", Toast.LENGTH_LONG).show();
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception exception) {
-                                           /*progressDialog.dismiss();
-                                Toast.makeText(context, exception.getMessage(), Toast.LENGTH_LONG).show();
-                          Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();*/
+                            progressDialog.dismiss();
+                            Toast.makeText(uploadActivity, exception.getMessage(), Toast.LENGTH_LONG).show();
+                            Toast.makeText(uploadActivity, exception.getMessage(), Toast.LENGTH_LONG).show();
                         }
                     })
                     .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
                             double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
-                            //progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
+                            progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
                         }
                     });
         }
