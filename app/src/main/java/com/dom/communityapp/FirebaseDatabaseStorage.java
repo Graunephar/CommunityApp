c50bd7f4215a8dac7dbeaaa100e2e71a4ab8d7af
MRL <mrlassen86@gmail.com>
2017-12-08 09:40:54 +0100

    Improved Image upload using Firebase Storage.

--- app/src/main/java/com/dom/communityapp/FirebaseDatabaseStorage.java
+++ app/src/main/java/com/dom/communityapp/FirebaseDatabaseStorage.java
@@ -2,16 +2,17 @@ package com.dom.communityapp;
 
 
 import android.app.ProgressDialog;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 
 /**
@@ -19,8 +20,8 @@ import com.google.firebase.storage.UploadTask;
  */
 
 public class FirebaseDatabaseStorage {
-
     UploadActivity uploadActivity;
+    private UploadTask uploadTask;
 
     public FirebaseDatabaseStorage(UploadActivity uploadActivity) {
         this.uploadActivity = uploadActivity;
@@ -30,44 +31,133 @@ public class FirebaseDatabaseStorage {
 
 
     public void uploadFile() {
-
+        //REF: https://www.simplifiedcoding.net/firebase-storage-tutorial-android/
+        //REF: https://theengineerscafe.com/firebase-storage-android-tutorial/
         if (uploadActivity.filePath != null) {
 
             final ProgressDialog progressDialog = new ProgressDialog(uploadActivity);
-            progressDialog.setTitle("Uploading");
+            progressDialog.setMax(100);
+            progressDialog.setMessage("Uploading...");
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
             progressDialog.show();
-
+            progressDialog.setCancelable(false);
 
             StorageReference imageRef = storageReference.child("images/" + uploadActivity.filePath.getLastPathSegment());
-            imageRef.putFile(uploadActivity.filePath)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
 
-                            progressDialog.dismiss();
-                            Toast.makeText(uploadActivity, "File Uploaded ", Toast.LENGTH_LONG).show();
-                            Toast.makeText(uploadActivity, "File Uploaded ", Toast.LENGTH_LONG).show();
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception exception) {
-                            progressDialog.dismiss();
-                            Toast.makeText(uploadActivity, exception.getMessage(), Toast.LENGTH_LONG).show();
-                            Toast.makeText(uploadActivity, exception.getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
-                            progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
-                        }
-                    });
+            uploadTask = imageRef.putFile(uploadActivity.filePath);
+
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                    //showing the uploaded image in ImageView using the download url. Choose this method or local method.
+                    Picasso.with(uploadActivity).load(downloadUrl).into(uploadActivity.img_view);
+
+                    progressDialog.dismiss();
+                    Toast.makeText(uploadActivity, "File Uploaded ", Toast.LENGTH_LONG).show();
+                }
+            });
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    progressDialog.dismiss();
+                    Toast.makeText(uploadActivity, exception.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+
+            uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
+                }
+            });
         }
-        //if there is not any file
+        //if there is no file
         else {
 
         }
     }
 }
+
+
+    /*
+    //REF: https://theengineerscafe.com/firebase-storage-android-tutorial/
+
+    private static final int SELECT_PHOTO = 100;
+    FirebaseStorage storage;
+    StorageReference storageRef, imageRef;
+    ProgressDialog progressDialog;
+    UploadTask uploadTask;
+    ImageView img_view;
+
+
+
+    EditText edit_description;
+    private Button btn_get_txt;
+    private TextView txt_get_txt;
+    Button btn_submit;
+    DatabaseReference rootRef, demoRef;
+
+
+    public Uri selectedImage;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+        img_view = (ImageView) findViewById(R.id.img_view);
+
+        edit_description = (EditText) findViewById(R.id.edit_description);
+        txt_get_txt = (TextView) findViewById(R.id.txt_get_txt);
+        btn_submit = (Button) findViewById(R.id.btn_submit);
+        btn_get_txt = (Button) findViewById(R.id.btn_get_txt);
+
+
+        //REF: https://theengineerscafe.com/save-and-retrieve-data-firebase-android/
+        //accessing the firebase storage
+        storage = FirebaseStorage.getInstance();
+        //creates a storage reference
+        storageRef = storage.getReference();
+
+        //ref pointing to root
+        rootRef = FirebaseDatabase.getInstance().getReference();
+
+        demoRef = rootRef.child("demo");
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String value = edit_description.getText().toString();
+                // Chose one or the other:
+
+                //creates a unique id in database
+                demoRef.push().setValue(value);
+
+                //creates one value, which is easy to fetch
+                demoRef.child("value").setValue(value);
+            }
+        });
+
+
+        btn_get_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                demoRef.child("value").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String value = dataSnapshot.getValue(String.class);
+                        txt_get_txt.setText(value);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+            }
+        });
+
+    }
+*/
--- app/src/main/java/com/dom/communityapp/UploadActivity.java
+++ app/src/main/java/com/dom/communityapp/UploadActivity.java
@@ -23,7 +23,7 @@ public class UploadActivity extends AppCompatActivity implements View.OnClickLis
     private Button buttonChoose;
     private Button buttonUpload;
 
-    private ImageView img_view;
+    public ImageView img_view;
     private FirebaseDatabaseStorage firebaseDatabaseStorage = new FirebaseDatabaseStorage(this);
     //used in Firebasedatabasestorage
     public Uri filePath;
@@ -46,14 +46,11 @@ public class UploadActivity extends AppCompatActivity implements View.OnClickLis
         buttonChoose.setOnClickListener(this);
         buttonUpload.setOnClickListener(this);
 
-
-
         //DATABASE
         edit_description = (EditText) findViewById(R.id.edit_description);
         txt_get_txt = (TextView) findViewById(R.id.txt_get_txt);
         btn_submit = (Button) findViewById(R.id.btn_submit);
         btn_get_txt = (Button) findViewById(R.id.btn_get_txt);
-
     }
 
     private void showFileChooser() {
@@ -70,6 +67,7 @@ public class UploadActivity extends AppCompatActivity implements View.OnClickLis
             filePath = data.getData();
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                //either set image locally from phone, or get it from firebase, using in FirebasaDatabaseStorage - Picasso.with(uploadActivity).load(downloadUrl).into(uploadActivity.img_view);
                 img_view.setImageBitmap(bitmap);
 
             } catch (IOException e) {
@@ -78,171 +76,12 @@ public class UploadActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
-
     @Override
     public void onClick(View view) {
         if (view == buttonChoose) {
             showFileChooser();
-        }
-        else if (view == buttonUpload) {
+        } else if (view == buttonUpload) {
             firebaseDatabaseStorage.uploadFile();
         }
     }
-
-
-
-
-
-
-
-    /*
-    //REF: https://theengineerscafe.com/firebase-storage-android-tutorial/
-
-    private static final int SELECT_PHOTO = 100;
-    FirebaseStorage storage;
-    StorageReference storageRef, imageRef;
-    ProgressDialog progressDialog;
-    UploadTask uploadTask;
-    ImageView img_view;
-
-
-
-    EditText edit_description;
-    private Button btn_get_txt;
-    private TextView txt_get_txt;
-    Button btn_submit;
-    DatabaseReference rootRef, demoRef;
-
-
-    public Uri selectedImage;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload);
-        img_view = (ImageView) findViewById(R.id.img_view);
-
-        edit_description = (EditText) findViewById(R.id.edit_description);
-        txt_get_txt = (TextView) findViewById(R.id.txt_get_txt);
-        btn_submit = (Button) findViewById(R.id.btn_submit);
-        btn_get_txt = (Button) findViewById(R.id.btn_get_txt);
-
-
-        //REF: https://theengineerscafe.com/save-and-retrieve-data-firebase-android/
-        //accessing the firebase storage
-        storage = FirebaseStorage.getInstance();
-        //creates a storage reference
-        storageRef = storage.getReference();
-
-        //ref pointing to root
-        rootRef = FirebaseDatabase.getInstance().getReference();
-
-        demoRef = rootRef.child("demo");
-
-        btn_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String value = edit_description.getText().toString();
-                // Chose one or the other:
-
-                //creates a unique id in database
-                demoRef.push().setValue(value);
-
-                //creates one value, which is easy to fetch
-                demoRef.child("value").setValue(value);
-            }
-        });
-
-
-        btn_get_txt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                demoRef.child("value").addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        String value = dataSnapshot.getValue(String.class);
-                        txt_get_txt.setText(value);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                    }
-                });
-            }
-        });
-
-    }
-
-    //REF: https://theengineerscafe.com/firebase-storage-android-tutorial/
-    public void selectImage(View view) {
-        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-        photoPickerIntent.setType("image");
-        startActivityForResult(photoPickerIntent, SELECT_PHOTO);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
-        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-        switch (requestCode) {
-            case SELECT_PHOTO:
-                if (resultCode == RESULT_OK) {
-                    Toast.makeText(UploadActivity.this, "Image selected, click on upload button", Toast.LENGTH_SHORT).show();
-                    selectedImage = imageReturnedIntent.getData();
-                }
-        }
-    }
-
-
-    //REF: https://theengineerscafe.com/firebase-storage-android-tutorial/
-    public void uploadImage(View view) {
-        //create reference to images folder and adding a name to the file that will be uploaded
-        imageRef = storageRef.child("images/" + selectedImage.getLastPathSegment());
-
-        //creating and showing progress dialog
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setMax(100);
-        progressDialog.setMessage("Uploading...");
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        progressDialog.show();
-        progressDialog.setCancelable(false);
-
-        //starting upload
-        uploadTask = imageRef.putFile(selectedImage);
-
-        // Observe state change events such as progress, pause, and resume
-        uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-
-            @Override
-            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
-                //sets and increments value of progressbar
-                progressDialog.incrementProgressBy((int) progress);
-            }
-        });
-
-        // Register observers to listen for when the download is done or if it fails
-        uploadTask.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle unsuccessful uploads
-                Toast.makeText(UploadActivity.this, "Error in uploading to firebase storage", Toast.LENGTH_SHORT).show();
-                progressDialog.dismiss();
-            }
-        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
-                Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                Toast.makeText(UploadActivity.this, "Upload successful to firebase storage", Toast.LENGTH_SHORT).show();
-                progressDialog.dismiss();
-                //showing the uploaded image in ImageView using the download url
-                Picasso.with(UploadActivity.this).load(downloadUrl).into(img_view);
-            }
-        });
-    }
-*/
-
-
-
 }
