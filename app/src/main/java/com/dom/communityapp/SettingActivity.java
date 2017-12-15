376ebfcdff4ae8b6a0c0ba6cb8f0ef910e9eb8af
oleklitgaard-jensen <Ole_kj@live.dk>
2017-12-15 15:37:21 +0100

    Layout tidbits

--- app/src/main/java/com/dom/communityapp/SettingActivity.java
+++ app/src/main/java/com/dom/communityapp/SettingActivity.java
@@ -3,8 +3,15 @@ package com.dom.communityapp;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SettingActivity extends AbstractNavigation {
+
+    SeekBar seekBar_id;
+    TextView seekBarValue;
+
 
     @Override
     protected DrawerLayout getdrawerLayout() {
@@ -20,5 +27,28 @@ public class SettingActivity extends AbstractNavigation {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_setting);
+
+    seekBar_id = (SeekBar) findViewById(R.id.SB_seekBar);
+    seekBarValue = (TextView) findViewById(R.id.txt_seekbar_value);
+
+
+  /*  seekBar_id.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            onStartTrackingTouch(seekBar);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            seekBarValue.setText(seekBar.getProgress());
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            seekBarValue.setText(seekBar.getProgress());
+        }
+
+    });*/
+
     }
 }
